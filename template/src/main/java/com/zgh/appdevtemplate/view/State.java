2e7f00b30f6d7633d825f0da8b058b02657784ef
Alex_zhao <zgh0711@163.com>
2017-06-15 14:36:57 +0800

    增加 popWindow,pickerView,SwipeMenuLayout,

---
+++ template/src/main/java/com/zgh/appdevtemplate/view/State.java
@@ -0,0 +1,12 @@
+package com.zgh.appdevtemplate.view;
+
+/**
+ * SwipeMenuLayout 带侧滑菜单功能的布局 所需要用到的枚举值
+ * Created by ZGH on 2017/6/15.
+ */
+
+public enum State {
+    LEFTOPEN,
+    RIGHTOPEN,
+    CLOSE,
+}
